93dba68ca4d02abb2a5e2edd931cd3470b863a45
hheimerd <hheimerd@gmail.com>
2023-09-24 12:24:00 +0200

    how it works

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Tagged templates https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
+console.log`
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+  ${'text'}
+  ${() => 'function'}
+`
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
